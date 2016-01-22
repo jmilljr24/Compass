8f35bd03dbcd43dd222eafd9b7c4ddf12f44524c
jmilljr24 <jmilljr24@gmail.com>
2016-01-22 09:23:53 -0500

    Create COMPASS64X13.h

---
+++ COMPASS64X13.h
@@ -0,0 +1,17 @@
+# ifndef COMPASS64X13_H
+# define COMPASS64X13_H
+# include <avr/pgmspace.h>
+
+extern const unsigned char compass360_bits[];
+extern const unsigned char compass1_bits[];
+extern const unsigned char compass2_bits[];
+extern const unsigned char compass3_bits[];
+extern const unsigned char compass4_bits[];
+extern const unsigned char compass5_bits[];
+extern const unsigned char compass6_bits[];
+extern const unsigned char compass7_bits[];
+extern const unsigned char compass8_bits[];
+extern const unsigned char compass9_bits[];
+extern const unsigned char compass10_bits[];
+
+#endif
